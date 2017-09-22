3e4f1e7a4a6ee84e46c8742d66e09abe884c9ea5
Nikolaas Verlee <nikotomad@gmail.com>
2017-09-22 20:40:46 +0200

    v2.0 with js + w3 validated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # npm
-Prework de Ironhack con la finalidad de crear un clon de la página web https://www.npmjs.com/
+Prework de Ironhack con la finalidad de crear un clon de la página web https://www.npmjs.com/ <br>
+Todo el código ha sido revisado con un validado de w3 en https://validator.w3.org/
---
+++ js/main.js
@@ -0,0 +1,6 @@
+function changeAcronym(){
+  var namelist = ['Never Push to Master','Neil Patricks Mansion','No Princess Mario','Nothing Prevents Misery','Node Package Manager','Naughty Push Message','Never Program Mad','Nineties Party Mix','Neverending Plethora of Modules'];
+  var acronym = document.getElementById("acronym");
+  acronym.innerHTML = namelist[Math.floor(Math.random() * namelist.length)]; // we use the array's length so that our Math.random maximum won't have to be updated in the future even if we add more names
+  console.log(namelist.length);
+}
