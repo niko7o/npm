3cb6c63a9c2c58d8d653d1587d554e8f8bf544d7
Nikolaas Verlee <nikotomad@gmail.com>
2017-09-24 03:29:13 +0200

    v2.23 cleaner main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 function changeAcronym(){
-  var namelist = ['Never Push to Master','Neil Patricks Mansion','No Princess Mario','Nothing Prevents Misery','Node Package Manager','Naughty Push Message','Never Program Mad','Nineties Party Mix','Neverending Plethora of Modules'];
+  var namelist = ['Never Push to Master','Neil Patricks Mansion','No Princess Mario',
+  'Nothing Prevents Misery','Node Package Manager','Naughty Push Message',
+  'Never Program Mad','Nineties Party Mix','Neverending Plethora of Modules'];
+
   var acronym = document.getElementById("acronym");
   // we use the array's length so that our Math.random maximum won't have
   // to be updated in the future even if we add more names
